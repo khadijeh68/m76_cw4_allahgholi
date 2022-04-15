f43532b6a9d8ea6ba069ab291420bc63145bea53
khadije <khadijeh.allahgholi@gmail.com>
2022-04-15 17:02:36 +0430

    q4 added

--- app1.js
+++ app1.js
@@ -33,12 +33,48 @@
 
 //q3 with map
 // function printArr(arr){
-//     arr.map(function(item , index) {
-//     console.log(`"row ${index}" \n`)
+//    arr.map(function(item , index) {
+//      console.log(`"row ${index}" \n`)
 //      console.log(`" ${item} " \n`);
-//     });
-//     }
-//     var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
-//     printArr(a);
+//    });
+//}
+//var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+//printArr(a);
+
+//q4
+// function concatArrs(arr){
+//     let concatedArr = arr.map(function(item, index){
+//         console.log(item)
+//         let res = item.reduce((acc, i) =>
+//             acc = acc+' ' +i
+//         )
+//         console.log(res);
+//     })
+// }
+// concatArrs([
+//     ["The","red", "horse"],
+//     ["Plane","over","the","ocean"],
+//     ["Chocolate","ice","cream","is","awesome"],
+//     ["this","is","a","long","sentence"]
+//     ])
+
+//q4 with return
+function concatArrs1(arr){
+    let concatedArr = arr.map(function(item, index){
+        let res = item.reduce((acc, i) => {
+            acc = acc+' ' +i;
+        return acc;
+        },'')
+        console.log(res);
+    })
+}
+concatArrs1([
+    ["The","red", "horse"],
+    ["Plane","over","the","ocean"],
+    ["Chocolate","ice","cream","is","awesome"],
+    ["this","is","a","long","sentence"]
+    ])      
+        
+    
 
 
