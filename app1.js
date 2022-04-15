b923dad64e5a87c4e4232f4b86f903b490cc1942
khadije <khadijeh.allahgholi@gmail.com>
2022-04-15 16:24:20 +0430

    q3 added

--- app1.js
+++ app1.js
@@ -10,11 +10,35 @@
 // }
 // lastNumber([7, 9, 0, -2],3);
 
-function myColor(arr){
-    let str = arr.join(',');
-    console.log(`" ${str} "`)
-    let str2 = arr.join('+');
-    console.log(`" ${str2} "`)
-}
-let array = ["Red", "Green", "White", "Black"]
-myColor(array);
+// function myColor(arr){
+//     let str = arr.join(',');
+//     console.log(`" ${str} "`)
+//     let str2 = arr.join('+');
+//     console.log(`" ${str2} "`)
+// }
+// let array = ["Red", "Green", "White", "Black"]
+// myColor(array);
+
+//q3
+// function myfunc(array){
+//     for (let i = 0 ; i <array.length ; i++){
+//         console.log("row" + i + ":");
+//         for (let j = 0 ; j <array.length-1 ; j++)
+//         console.log(array[i][j])
+        
+//     }  
+// }
+// var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+// myfunc(a);
+
+//q3 with map
+// function printArr(arr){
+//     arr.map(function(item , index) {
+//     console.log(`"row ${index}" \n`)
+//      console.log(`" ${item} " \n`);
+//     });
+//     }
+//     var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+//     printArr(a);
+
+
